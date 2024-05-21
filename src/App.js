55522e2eff0c913a37a028e347a151dc61fbb150
tahyo27 <tahyo27@gmail.com>
2024-05-21 19:25:02 +0900

    animal 베이스

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
+import { useState ,useEffect } from "react";
 import './App.css';
 
 function App() {
+  const[animalData, setAnimalData] = useState([]);
+
+  useEffect(() => {
+    const getAnimalData = async () => {
+      try {
+        const url = `http://34.22.99.244:8071/animals`;
+        const response = await fetch(url);
+        const jsonData = await response.json();
+        setAnimalData(jsonData);
+      } catch(error) {
+        console.log("animal API fetch error", error);
+      }
+    }
+    getAnimalData();
+  }, []) 
+
+  console.log(animalData);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {animalData.length > 0 ? (
+        <ul className="list">
+          {animalData.map(animal => (
+            <li key={animal.id} className="list-item">
+              <img src={animal.imgUrl} alt={animal.name} className="image" />
+              <h2>{animal.name}</h2>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <h2>Loading animal data...</h2>
+      )}
     </div>
   );
 }
